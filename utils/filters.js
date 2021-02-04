291515ec69a47d0a918a2f9786a9fbcfa2be0541
Vadzim Tsupryk <v.tsupryk@mageworx.com>
2021-02-04 13:55:43 +0300

    Adjust footer

--- utils/filters.js
+++ utils/filters.js
@@ -8,4 +8,16 @@ module.exports = {
 
   log: (data) => console.log(`\n\n${util.inspect(data)}\n\n`),
   markdown: (content) => markdown.renderInline(content),
+  strip_headings: (content) => {
+    return content.replace(/<h\d[^>]*?>(\r\n|\r|\n|.)*?<\/h\d>/g, '');
+  },
+  strip_code: (content) => {
+    return content.replace(/<pre>(\r\n|\r|\n|.)*?<\/pre>/g, '');
+  },
+  strip_extra_whitespaces: (content) => {
+    return content.replace(/\s{2,}/g, ' ');
+  },
+  strip_newlines: (content) => {
+    return content.replace(/(\r\n|\n|\r)/g, ' ');
+  },
 };

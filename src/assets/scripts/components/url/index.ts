d4d1ab583a4d9a023891860bc554f3cdf2c5daef
Vadzim Tsupryk <v.tsupryk@mageworx.com>
2021-04-12 11:26:45 +0300

    Hide footer conditionally. Adjust main content styles

--- src/assets/scripts/components/url/index.ts
+++ src/assets/scripts/components/url/index.ts
@@ -1,12 +1,14 @@
 import { fromEvent } from 'rxjs';
 
 export function initializeEmbedded() {
-  fromEvent(window, 'load').subscribe(() => {
+  fromEvent(document, 'DOMContentLoaded').subscribe(() => {
     const params = new URLSearchParams(window.location.search);
     const header = document.querySelector('.header');
+    const footer = document.querySelector('.footer');
 
-    if (params.get('embedded') === null && header) {
+    if (params.get('embedded') === null && header && footer) {
       header.classList.remove('header--hidden');
+      footer.classList.remove('footer--hidden');
     }
   });
 }
